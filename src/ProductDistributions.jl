6adf76b5c00ebc277d1aec74a0ad0026d8ba56d2
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2016-09-21 15:03:50 +0200

    First commit

--- src/ProductDistributions.jl
+++ src/ProductDistributions.jl
@@ -1,5 +1,59 @@
 module ProductDistributions
 
-# package code goes here
+using Reexport
+import Distributions: ContinuousMultivariateDistribution
+@reexport using Distributions
+
+immutable ProductDistribution <: ContinuousMultivariateDistribution
+    ## Assign a prior on α, β such that
+    marginals::Array{ContinuousDistribution, 1}
+end
+
+Base.length(d::ProductDistribution) = length(d.marginals)
+
+function ProductDistribution(args...)
+    ProductDistribution([args...])
+end
+
+function Distributions.insupport{T<:Real}(d::ProductDistribution, x::AbstractVector{T})
+    all(map(insupport, d.marginals, x))
+end
+
+# function logpdf{T<:Real}(d::ProductDistribution, x::AbstractVector{T})
+#     if Distributions.insupport(d, x)
+#         l = zero(T)
+#         @inbounds for i = 1:length(d.marginals)
+#             l += logpdf(d.marginals[i], x[i])
+#         end
+#     else
+#         l = convert(T, -Inf)
+#     end
+#     l
+# end
+
+function Distributions.logpdf{T<:Real}(d::ProductDistribution, x::AbstractVector{T})
+    if Distributions.insupport(d, x)
+        l = zero(T)
+        @inbounds for i = 1:length(d.marginals)
+            l += logpdf(d.marginals[i], x[i])
+        end
+    else
+        l = convert(T, -Inf)
+    end
+    l
+end
+
+function Distributions.rand!{T<:Real}(d::ProductDistribution, x::DenseMatrix{T})
+    P, N = size(x)
+    @inbounds for i = 1:N
+        for p = 1:P
+            x[p, i] = rand(d.marginals[p])
+        end
+    end
+    x
+end
+
+export ProductDistribution
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using ProductDistributions
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+using ProductDistributions
+
+const T = typeof(1.0)
+
+
+d = ProductDistribution(Normal(0,1), Uniform(0,1))
+
+n = 10000
+
+x = Array(T, 2, n)
+
+rand!(d, x)
+
+μ = mean(x, 2)
+σ²= var(x, 2)
+
+@test isa(μ, Array{T, 2})
+@test isa(σ², Array{T, 2})
+
+@test !insupport(d, [0., 2.])
+@test insupport(d, [0., 1.])
+
+@test logpdf(d, [0., 0.5]) .== logpdf(d.marginals[1], 0.) + logpdf(d.marginals[2], 0.5)
