b22033c9661c76fd7a1fb8754aea48bc6dcb449a
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2018-09-05 14:23:36 +0200

    Drop Julia 0.6 support

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,22 @@
-using Base.Test
+using Test
 using BayesianTools
 using StatsFuns
-
+using Random
 const T = typeof(1.0)
 
 using BayesianTools.ProductDistributions
 
-d = ProductDistribution(Normal(0,1), Uniform(0,1), InverseGamma(0.01, 0.01))
+d1 = ProductDistribution(Normal(0,1), Uniform(0,1), InverseGamma(0.01, 0.01))
 
-@test length(d) == 3
-@test insupport(d, [0., 1., 1.])
-@test insupport(d, [0., .1, 21.])
-@test !insupport(d, [0., 2., .1])
-@test !insupport(d, [0., -2., .1])
-@test !insupport(d, [0., .1, -2])
+@test length(d1) == 3
+@test insupport(d1, [0., 1., 1.])
+@test insupport(d1, [0., .1, 21.])
+@test !insupport(d1, [0., 2., .1])
+@test !insupport(d1, [0., -2., .1])
+@test !insupport(d1, [0., .1, -2])
 
-@test logpdf(d, [0., 0.5, 0.5]) .== logpdf(d.marginals[1], 0.) + logpdf(d.marginals[2], 0.5) + logpdf(d.marginals[3], 0.5)
-@test pdf(d, [0., 0.5, 0.5]) ≈ pdf(d.marginals[1], 0.)*pdf(d.marginals[2], 0.5)*pdf(d.marginals[3], 0.5)
+@test logpdf(d1, [0., 0.5, 0.5]) .== logpdf(d1.marginals[1], 0.) + logpdf(d1.marginals[2], 0.5) + logpdf(d1.marginals[3], 0.5)
+@test pdf(d1, [0., 0.5, 0.5]) ≈ pdf(d1.marginals[1], 0.)*pdf(d1.marginals[2], 0.5)*pdf(d1.marginals[3], 0.5)
 
 d1 = Improper(0, +Inf)
 d2 = Improper(-Inf, +Inf)
@@ -68,13 +68,13 @@ for d in [Normal(0,1), TDist(2)]
     @test invlink(d, link(d, 0.1)) ≈ 0.1
 end
 d = ProductDistribution(Normal(0,1), Uniform(0,1), InverseGamma(0.01, 0.01))
-@test link(d, invlink(d, [0.0, -1., 2.])) == [0.0, -1., 2.]
+@test link(d, invlink(d, [0.0, -1., 2.])) ≈ [0.0, -1., 2.] atol=1e-15
 
 d = ProductDistribution(Normal(0,1), Uniform(0,1), InverseGamma(0.01, 0.01), Improper(0,+Inf))
-@test link(d, invlink(d, [0.0, -1., 2., -2.])) == [0.0, -1., 2., -2.]
+@test link(d, invlink(d, [0.0, -1., 2., -2.])) ≈ [0.0, -1., 2., -2.] atol=1e-15
 
 d = ProductDistribution(Normal(), Uniform())
-srand(1)
+Random.seed!(1)
 x2 = hcat([rand( d ) for i in 1:1000]...)
 @test maximum(x2[2,:])<=1.0
 @test maximum(x2[1,:])>=0.0
